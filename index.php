f364cacd7b08c562ff7f509ceb632044cef81077
brunodlj <161373268+brunodlj@users.noreply.github.com>
2024-06-14 13:50:36 -0300

    exibir imagem

--- index.php
+++ index.php
@@ -27,7 +27,9 @@ if($resultado != false){
 </form>
     <table>
         <tr> 
-            <th> Nome do Arquivo </th>
+
+            <th colsplan="2"> imagem do Arquivo </th>
+            <th colsplan="2"> Arquivo </th> <br>
             <th colspan = "2"> Opções </td>
         </tr>
     <thead>
@@ -36,7 +38,8 @@ if($resultado != false){
         foreach($arquivos as $arquivos){
             $arq = $arquivos['nome_arquivo'];
             echo "<tr>"; //iniciar a linha
-            echo "<td>".$arq."</td>"; //primeira coluna com o nome do arquivo 
+            echo "<td><img src='uploads/$arq' width='100px' height='100px'> </td>"; //Exibe a imagem
+            echo "<td> <a href='uploads/$arq'>$arq</a></td>"; //primeira coluna com o nome do arquivo 
             echo "<td>"; // iniciar a segunda coluna
             echo "<a "; //abriu o link (abriu a tag <a>)
             echo "href ='alterar.php?nome_arquivo=".$arq."'>";//inseriu o link
